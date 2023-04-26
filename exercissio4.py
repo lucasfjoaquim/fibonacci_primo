ab7274693a48a0ae060f4d61952e1fd518191b3c
lucasfjoaquim <lucasfjoaquim@gmail.com>
2023-04-25 22:35:50 -0300

    quarto exercissio

---
+++ exercissio4.py
@@ -0,0 +1,6 @@
+listaDeProfessor = ["Cordeiro","Danilo","Israel","Allem"]
+for i in listaDeProfessor:
+    if i == "Danilo":
+        print(f"{i} é bem foda")
+    else:
+        print(f"{i} não é tão foda")
